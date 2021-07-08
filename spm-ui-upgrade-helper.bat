553c50c994c91a0c78d2703618dcf062e2ec0969
Alan Bergin <alan.bergin@ie.ibm.com>
2021-07-08 14:04:24 +0100

    dev.bat now calls spm-ui-upgrade-helper.bat

--- spm-ui-upgrade-helper.bat
+++ spm-ui-upgrade-helper.bat
@@ -17,18 +17,18 @@ if not "%ERROR%"=="" (
 set INPUT_FOLDER=%1
 set OUTPUT_FOLDER=%2
 
-if "%ADDITIONAL_RULES%" == "" (
+if "%3" == "" (
   set ADDITIONAL_RULES_CMD=
 ) else (
   set ADDITIONAL_RULES_CMD=-v %ADDITIONAL_RULES%:/home/workspace/rules
 )
-if "%ADDITIONAL_IGNORE%" == "" (
+if "%4" == "" (
   set ADDITIONAL_IGNORE_CMD=
 ) else (
   set ADDITIONAL_IGNORE_CMD=-v %ADDITIONAL_IGNORE%:/home/workspace/ignore
 )
 
-echo starting spm-ui-upgrade-helper
+echo Starting spm-ui-upgrade-helper
 echo.
 echo     INPUT_FOLDER = %INPUT_FOLDER%
 echo     OUTPUT_FOLDER = %OUTPUT_FOLDER%
@@ -38,14 +38,14 @@ echo.
 
 call docker stop spm-ui-upgrade-helper
 call docker rm spm-ui-upgrade-helper
-call docker run -p 3000:3000 -p 4000-4002:4000-4002 -v %INPUT_FOLDER%:/home/workspace/input -v %OUTPUT_FOLDER%:/home/workspace/output %ADDITIONAL_RULES_CMD% %ADDITIONAL_IGNORE_CMD% --name spm-ui-upgrade-helper wh-govspm-docker-local.artifactory.swg-devops.com/artifactory/wh-govspm-docker-local/spm-ui-upgrade-helper/spm-ui-upgrade-helper:latest
+call docker run -p 3000:3000 -p 4000-4002:4000-4002 %UIUH_DEV_CMD% -v %INPUT_FOLDER%:/home/workspace/input -v %OUTPUT_FOLDER%:/home/workspace/output %ADDITIONAL_RULES_CMD% %ADDITIONAL_IGNORE_CMD% --name spm-ui-upgrade-helper wh-govspm-docker-local.artifactory.swg-devops.com/artifactory/wh-govspm-docker-local/spm-ui-upgrade-helper/spm-ui-upgrade-helper:latest
 
 endlocal
 
 goto end
 
 :printHelpAndExit
-echo Usage: spm-ui-upgrade-helper.bat ^<input folder^> ^<output folder^>
+echo Usage: spm-ui-upgrade-helper.bat ^<input folder^> ^<output folder^> [^<additional rules^>] [^<additional ignore^>]
 exit /B 1
 
 :end
