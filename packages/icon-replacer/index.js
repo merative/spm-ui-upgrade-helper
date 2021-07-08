6e13edb921bb30e3bd754489a61d0dc34144299e
Jonathan Byrne <jonathan.byrne@ibm.com>
2021-07-08 15:53:48 +0100

    removes personal dev files

--- packages/icon-replacer/index.js
+++ packages/icon-replacer/index.js
@@ -21,15 +21,4 @@ const execute = () => {
   );
 };
 
-// this works 👇
-
-// const config = utils.loadConfig();
-// iconReplacer.run(
-//   "/Users/jonathan/Code/SPM/70110-Master/streams/TI",
-//   "/Users/jonathan/Code/spm-ui-upgrade-helper/packages/icon-replacer/source_files",
-//   "/Users/jonathan/Code/spm-ui-upgrade-helper/packages/icon-replacer/icon_mappings.json",
-//   config.iconReferenceExclude,
-//   config.verbose
-// );
-
 module.exports = { execute };
