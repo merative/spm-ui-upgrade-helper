5de88538fd6af1aff97fb6b9ac31f7635f73e3d0
Alan Bergin <alan.bergin@ie.ibm.com>
2021-07-08 16:03:03 +0100

    Refactor removeIgnoredFiles code for clarity

--- packages/shared-utils/src/removeIgnoredFiles.js
+++ packages/shared-utils/src/removeIgnoredFiles.js
@@ -1,5 +1,17 @@
 const fileio = require('@folkforms/file-io');
 
+const containsAnyToken = (path, tokens) => {
+  for(let i = 0; i < tokens.length; i++) {
+    const token = tokens[i];
+    const index = path.indexOf(token);
+    if(index != -1) {
+      console.log(`## ignoring (via token '${token}'): ${path}`);
+      return true;
+    }
+  }
+  return false;
+}
+
 const removeIgnoredFiles = (config, inputFiles) => {
   const ignoreFiles = [
     fileio.glob(`${config.ignorePatternsFolder}/*.json`),
@@ -10,26 +22,21 @@ const removeIgnoredFiles = (config, inputFiles) => {
     const ignoreJson = fileio.readJson(filename);
     // Ignore tokens
     console.log(`#### inputFiles = ${JSON.stringify(inputFiles)}`);
-    for(let i = 0; i < inputFiles.length; i++) {
-      if(ignoreJson.tokens && ignoreJson.tokens.length > 0) {
-        ignoreJson.tokens.forEach(token => {
-          const index = inputFiles[i].indexOf(token);
-          if(index != -1) {
-            console.log(`## ignoring (via token '${token}'): ${inputFiles[i]}`);
-            inputFiles.splice(i, 1);
-            i--;
-            return;
-          }
-        });
+    if(ignoreJson.tokens && ignoreJson.tokens.length > 0) {
+      for(let i = 0; i < inputFiles.length; i++) {
+        if(containsAnyToken(inputFiles[i], ignoreJson.tokens)) {
+          inputFiles.splice(i, 1);
+          i--;
+        }
       }
     }
     // Ignore globs
     if(ignoreJson.globs && ignoreJson.globs.length > 0) {
       ignoreJson.globs.forEach(pattern => {
         const glob = `${config.inputFolder}/${pattern}`;
-        const foundFiles = fileio.glob(glob);
-        foundFiles.forEach(foundFile => {
-          let index = inputFiles.indexOf(foundFile);
+        const globbedFiles = fileio.glob(glob);
+        globbedFiles.forEach(globbedFile => {
+          let index = inputFiles.indexOf(globbedFile);
           if(index !== -1) {
             console.log(`## ignoring (via glob '${glob}'): ${inputFiles[index]}`);
             inputFiles.splice(index, 1);
