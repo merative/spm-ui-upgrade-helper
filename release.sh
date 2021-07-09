84a29736887a492299d41b5b12fc4ca49974c1c5
Alan Bergin <alan.bergin@ie.ibm.com>
2021-07-09 12:10:30 +0100

    Fix error in release.sh

--- release.sh
+++ release.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/sh
 
-echo ==== release.sh ====
-
 version=`git tag --points-at HEAD`
 if [ -z $version ]; then echo "No version tag found."; exit 0; fi
 echo found tag = $version
@@ -20,10 +18,9 @@ if [ "$?" != 0 ]; then echo "Build Failed"; exit 1; fi
 
 yarn docker-tasks release $version -n
 if [ "$?" != 0 ]; then echo "Build Failed"; exit 1; fi
+
 #docker image tag spm-ui-upgrade-helper:latest spm-ui-upgrade-helper:0.0.0
 #docker image tag spm-ui-upgrade-helper:latest wh-govspm-docker-local.artifactory.swg-devops.com/artifactory/wh-govspm-docker-local/spm-ui-upgrade-helper/spm-ui-upgrade-helper:0.0.0
 #docker image tag spm-ui-upgrade-helper:latest wh-govspm-docker-local.artifactory.swg-devops.com/artifactory/wh-govspm-docker-local/spm-ui-upgrade-helper/spm-ui-upgrade-helper:latest
 #docker image push wh-govspm-docker-local.artifactory.swg-devops.com/artifactory/wh-govspm-docker-local/spm-ui-upgrade-helper/spm-ui-upgrade-helper:0.0.0
 #docker image push wh-govspm-docker-local.artifactory.swg-devops.com/artifactory/wh-govspm-docker-local/spm-ui-upgrade-helper/spm-ui-upgrade-helper:latest
-
-echo "All done!"
