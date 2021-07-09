65074cd8578e6072aecd85a270c8c99ce245564e
Alan Bergin <alan.bergin@ie.ibm.com>
2021-07-09 15:42:56 +0100

    Remove commented-out code

--- release/release-cli.js
+++ release/release-cli.js
@@ -4,13 +4,14 @@ const { Command } = require('commander');
 const release = require("./release-local");
 
 const program = new Command();
-program.name("yarn release");
-program.usage("--start/--ship <version>");
-program.addHelpText('beforeAll', 'Create a release for testing, or ship the current release.\n');
-program.option('--start <version>', 'Start the release process by creating and pushing the release branch and building a docker image');
-program.option('--ship <version>', 'Ship the release by pushing the latest docker image to the repo and tagging the current commit');
-program.option('-n, --dry-run', 'Show what commands would be executed, without actually running anything');
-program.parse(process.argv);
+program
+  .name("yarn release")
+  .usage("--start/--ship <version>")
+  .addHelpText('beforeAll', 'Create a release for testing, or ship the current release.\n')
+  .option('--start <version>', 'Start the release process by creating and pushing the release branch and building a docker image')
+  .option('--ship <version>', 'Ship the release by pushing the latest docker image to the repo and tagging the current commit')
+  .option('-n, --dry-run', 'Show what commands would be executed, without actually running anything')
+  .parse(process.argv);
 
 const opts = program.opts();
 const start = opts.start;
@@ -19,10 +20,6 @@ const dryRun = opts.dryRun;
 
 if(!start && !ship) {
   program.help();
-  // console.log("ERROR: You must supply an option and a version as params");
-  // console.log("Usage: yarn release --start/--ship <version> [-n/--dry-run]");
-  // console.log("Example: yarn release --start 0.0.1");
-//  return 1;
 }
 
 let option, version;
