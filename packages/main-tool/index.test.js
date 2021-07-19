054e2c05a58b8eeb148b28d93b019d0265aa8a1b
Alan Bergin <alan.bergin@ie.ibm.com>
2021-07-19 16:26:07 +0100

    Convert main-tools tests to a loop over test-data folders

--- packages/main-tool/index.test.js
+++ packages/main-tool/index.test.js
@@ -1,46 +1,31 @@
+const fileio = require("@folkforms/file-io");
 const testWithDataFolder = require("test-with-data-folder");
 const { execute } = require("./index");
 
-const testToolOverrides = [
-  {
-    "package": "main-tool",
-    "name": "Run SPM UI Upgrade Helper",
-    "port": 4000,
-    "commandName": "extension.runUIUpgradeHelperMain",
-    "enabled": true
-  },
-  {
-    "package": "main-tool/test-data/stubs/fake-tool-1",
-    "name": "Run SPM UI Upgrade Helper - Fake Tool 1",
-    "port": 4001,
-    "commandName": "extension.runUIUpgradeHelperFakeTool1",
-    "enabled": true
-  },
-  {
-    "package": "main-tool/test-data/stubs/fake-tool-2-disabled",
-    "name": "Run SPM UI Upgrade Helper - Fake Tool 2",
-    "port": 4002,
-    "commandName": "extension.runUIUpgradeHelperFakeTool2",
-    "enabled": false
-  },
-  {
-    "package": "main-tool/test-data/stubs/fake-tool-3",
-    "name": "Run SPM UI Upgrade Helper - Fake Tool 3",
-    "port": 4003,
-    "commandName": "extension.runUIUpgradeHelperFakeTool3",
-    "enabled": true
-  }
-];
-
-const inputFolder = "./test-data/test-case-1/input";
-const expectedFolder = "./test-data/test-case-1/expected";
-const temporaryFolder = "./test-data/test-case-1/temp";
-const testConfigOverrides = {
-  inputFolder,
-  outputFolder: temporaryFolder,
-};
-
-test('main-tool index test', () => {
-  const testFunc = () => { execute(testConfigOverrides, testToolOverrides); };
-  testWithDataFolder(testFunc, inputFolder, expectedFolder, temporaryFolder);
+/**
+ * Run the test against each of the test case folders.
+ */
+const testCaseFolders = fileio.glob("test-data/test-case-*", { onlyDirectories: true, deep: 1 });
+testCaseFolders.forEach(folder => {
+  test(`main-tool test (from: ${folder})`, () => {
+    runTest(folder);
+  });
 });
+
+/**
+ * Runs a test using the test data from the given folder.
+ */
+const runTest = folder => {
+  const inputFolder = `${folder}/input`;
+  const expectedFolder = `${folder}/expected`;
+  const temporaryFolder = `${folder}/temp`;
+  const testToolsOverride = fileio.readJson(`${folder}/tools.json`);
+  const testConfigOverrides = {
+    inputFolder,
+    outputFolder: temporaryFolder,
+  };
+
+  const testFunc = () => { execute(testConfigOverrides, testToolsOverride); };
+
+  testWithDataFolder(testFunc, inputFolder, expectedFolder, temporaryFolder);
+}
