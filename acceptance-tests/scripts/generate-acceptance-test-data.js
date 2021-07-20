b4ef3a43dc993e21104a420f9e511e17c147f3aa
Alan Bergin <alan.bergin@ie.ibm.com>
2021-07-20 09:15:14 +0100

    Acceptance test generated files now include icon changes

--- acceptance-tests/scripts/generate-acceptance-test-data.js
+++ acceptance-tests/scripts/generate-acceptance-test-data.js
@@ -3,30 +3,73 @@
  */
 const shelljs = require("shelljs");
 
-const noUpdatesCss = 300;
-const hasUpdatesCss = 7;
+const filesPerFolder = 50;
+const noUpdatesCssCount = 300;
+const hasUpdatesCssCount = 7;
+const hasUpdatesIconsCount = 4;
+const actualIconFiles = 2;
+const hasUpdatesCssAndIconsCount = 2;
+
+// Create dummy files with no changes
+folder = -1;
+const addUnchangedFiles = count => {
+  for(let i = 0; i < count; i++) {
+    if(i % filesPerFolder === 0) {
+      folder++;
+      shelljs.mkdir("-p", `acceptance-tests/input/${folder}`);
+    }
+    shelljs.cp("acceptance-tests/scripts/dummy-files/no-updates.css", `acceptance-tests/input/${folder}/no-updates-${i}.css`);
+  }
+}
+
+const getFilenameWithIndex = (file, count) => {
+  const dotIndex = file.lastIndexOf(".");
+  const name = file.substring(0, dotIndex);
+  const ext = file.substring(dotIndex + 1);
+  return `${name}-${count}.${ext}`;
+}
+
+/**
+ * Adds a copy of the given file into each folder sequentially.
+ *
+ * @param {*} file file to copy
+ * @param {*} count number of files to create
+ */
+const addFiles = (file, count) => {
+  let folderIndex = 0;
+  for(let i = 0; i < count; i++) {
+    shelljs.cp(`acceptance-tests/scripts/dummy-files/${file}`, `acceptance-tests/input/${folderIndex}/${getFilenameWithIndex(file, i)}`);
+    folderIndex++;
+    if(folderIndex > folder) {
+      folderIndex = 0;
+    }
+  }
+}
+
+/**
+ * Adds a copy of the given file into each folder sequentially, without renaming.
+ *
+ * @param {*} file file to copy
+ * @param {*} count number of files to create
+ */
+const copyFiles = (file, count) => {
+  let folderIndex = 0;
+  for(let i = 0; i < count; i++) {
+    shelljs.cp(`acceptance-tests/scripts/dummy-files/${file}`, `acceptance-tests/input/${folderIndex}/${file}`);
+    folderIndex++;
+    if(folderIndex > folder) {
+      folderIndex = 0;
+    }
+  }
+}
 
 shelljs.rm("-rf", `acceptance-tests/input`);
 shelljs.rm("-rf", `acceptance-tests/output`);
 shelljs.mkdir(`acceptance-tests/input`);
 shelljs.mkdir(`acceptance-tests/output`);
 
-// Create dummy files with no changes
-folder = -1;
-for(let i = 0; i < noUpdatesCss; i++) {
-  if(i % 100 === 0) {
-    folder++;
-    shelljs.mkdir("-p", `acceptance-tests/input/${folder}`);
-  }
-  shelljs.cp("acceptance-tests/scripts/dummy-files/no-updates.css", `acceptance-tests/input/${folder}/no-updates-${i}.css`);
-}
-
-// Drop a copy of "has-updates.css" into each folder sequentially until i >= hasUpdatesCss.
-let folderIndex = 0;
-for(let i = 0; i < hasUpdatesCss; i++) {
-  shelljs.cp("acceptance-tests/scripts/dummy-files/has-updates.css", `acceptance-tests/input/${folderIndex}/has-updates-${i}.css`);
-  folderIndex++;
-  if(folderIndex > folder) {
-    folderIndex = 0;
-  }
-}
+addUnchangedFiles(noUpdatesCssCount);
+addFiles("has-updates-css.css", hasUpdatesCssCount);
+addFiles("has-updates-icons.properties", hasUpdatesIconsCount);
+addFiles("has-updates-css-and-icons.css", hasUpdatesCssAndIconsCount);
+copyFiles("Chevron_Down_Blue30_10px.png", actualIconFiles);
