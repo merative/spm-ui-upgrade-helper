f1c5ad717e854c8def1af3e4bf8a60b6464bb621
Alan Bergin <alan.bergin@ie.ibm.com>
2021-07-22 12:12:19 +0100

    Create nested folders during acceptance tests

--- acceptance-tests/scripts/generate-acceptance-test-data.js
+++ acceptance-tests/scripts/generate-acceptance-test-data.js
@@ -12,54 +12,87 @@ program
 const allDataSets = fileio.readJson("acceptance-tests/scripts/acceptance-test-data-sets.json");
 const data = allDataSets[program.args[0]];
 
-// Create dummy files with no changes
-folder = -1;
-const addUnchangedFiles = count => {
-  for(let i = 0; i < count; i++) {
-    if(i % data.filesPerFolder === 0) {
-      folder++;
-      shelljs.mkdir("-p", `acceptance-tests/input/${folder}`);
+/**
+ * Generate nested test folders.
+ *
+ * @param {*} numFolders total number of folders
+ * @param {*} folderDepth maximum folder depth
+ */
+const createFolders = (numFolders, folderDepth) => {
+  const folders = [];
+  const topLevelFolders = numFolders / folderDepth;
+  let count = 0;
+  let path = "";
+  for(let i = 0 ; i < topLevelFolders; i++) {
+    for(let j = 0 ; j < data.folderDepth; j++) {
+      path += `${count}/`;
+      folders.push(path);
+      count++;
     }
-    shelljs.cp("acceptance-tests/scripts/dummy-files/no-updates.css", `acceptance-tests/input/${folder}/no-updates-${i}.css`);
+    path = "";
   }
-}
-
-const getFilenameWithIndex = (file, count) => {
-  const dotIndex = file.lastIndexOf(".");
-  const name = file.substring(0, dotIndex);
-  const ext = file.substring(dotIndex + 1);
-  return `${name}-${count}.${ext}`;
+  for(let i = 0 ; i < folders.length; i++) {
+    shelljs.mkdir("-p", `acceptance-tests/input/${folders[i]}`);
+  }
+  return folders;
 }
 
 /**
- * Adds a copy of the given file into each folder sequentially.
+ * Add files that have no changes into each of the folders sequentially.
  *
- * @param {*} file file to copy
- * @param {*} count number of files to create
+ * @param {*} count number of files to add
+ * @param {*} folders list of folders
  */
-const addFiles = (file, count) => {
-  let folderIndex = 0;
+const addUnchangedFiles = (count, folders) => {
   for(let i = 0; i < count; i++) {
-    shelljs.cp(`acceptance-tests/scripts/dummy-files/${file}`, `acceptance-tests/input/${folderIndex}/${getFilenameWithIndex(file, i)}`);
+    shelljs.cp("acceptance-tests/scripts/dummy-files/no-updates.css", `acceptance-tests/input/${folders[folderIndex]}/no-updates-${i}.css`);
     folderIndex++;
-    if(folderIndex > folder) {
+    if(folderIndex >= folders.length) {
       folderIndex = 0;
     }
   }
 }
 
 /**
- * Adds a copy of the given file into each folder sequentially, without renaming.
+ * Inserts an index into a filename, e.g. `foo.txt` => `foo-1.txt`.
+ *
+ * @param {*} file filename
+ * @param {*} index index to insert
+ */
+const getFilenameWithIndex = (file, index) => {
+  const dotIndex = file.lastIndexOf(".");
+  const name = file.substring(0, dotIndex);
+  const ext = file.substring(dotIndex + 1);
+  return `${name}-${index}.${ext}`;
+}
+
+/**
+ * Adds a copy of the given file into each of the folders sequentially.
  *
  * @param {*} file file to copy
  * @param {*} count number of files to create
  */
-const copyFiles = (file, count) => {
-  let folderIndex = 0;
+const addFiles = (file, count, folders) => {
   for(let i = 0; i < count; i++) {
-    shelljs.cp(`acceptance-tests/scripts/dummy-files/${file}`, `acceptance-tests/input/${folderIndex}/${file}`);
+    shelljs.cp(`acceptance-tests/scripts/dummy-files/${file}`, `acceptance-tests/input/${folders[folderIndex]}/${getFilenameWithIndex(file, i)}`);
     folderIndex++;
-    if(folderIndex > folder) {
+    if(folderIndex >= folders.length) {
+      folderIndex = 0;
+    }
+  }
+}
+
+/**
+ * Adds a copy of the given file into each of the folders sequentially, without renaming.
+ *
+ * @param {*} file file to copy
+ * @param {*} count number of files to create
+ */
+const copyFiles = (file, count, folders) => {
+  for(let i = 0; i < count; i++) {
+    shelljs.cp(`acceptance-tests/scripts/dummy-files/${file}`, `acceptance-tests/input/${folders[folderIndex]}/${file}`);
+    folderIndex++;
+    if(folderIndex >= folders.length) {
       folderIndex = 0;
     }
   }
@@ -85,9 +118,11 @@ shelljs.rm("-rf", `acceptance-tests/output`);
 shelljs.mkdir(`acceptance-tests/input`);
 shelljs.mkdir(`acceptance-tests/output`);
 
-addUnchangedFiles(data.filesWithNoUpdates);
-addFiles("has-updates-css.css", data.filesWithCssUpdates);
-addFiles("has-updates-icons.properties", data.filesWithIconUpdates);
-copyFiles("Chevron_Down_Blue30_10px.png", data.iconFilesToUpdate);
-addFiles("has-updates-css-and-icons.css", data.filesWithCssAndIconUpdates);
+let folderIndex = 0;
+const folders = createFolders(data.numFolders, data.folderDepth);
+addUnchangedFiles(data.filesWithNoUpdates, folders);
+addFiles("has-updates-css.css", data.filesWithCssUpdates, folders);
+addFiles("has-updates-icons.properties", data.filesWithIconUpdates, folders);
+copyFiles("Chevron_Down_Blue30_10px.png", data.iconFilesToUpdate, folders);
+addFiles("has-updates-css-and-icons.css", data.filesWithCssAndIconUpdates, folders);
 addIgnoredFiles();
