fe5ee2e3dff577033c3104c3ba7d777182daf557
Alan Bergin <alan.bergin@ie.ibm.com>
2021-07-23 12:14:04 +0100

    Add prototype for js-rules-tool

--- packages/js-rules-tool/index.js
+++ packages/js-rules-tool/index.js
@@ -5,9 +5,9 @@ const utils = require("../shared-utils/sharedUtils");
 const { applyRulesToJS } = require("./src/applyRulesToJS");
 
 /**
- * Main method. Will be called via http://localhost:4002/execute
+ * Main method. Will be called via http://localhost:40xx/execute
  */
-const execute = () => {
+const execute = overrides => {
   // estraverse.traverse(x2, {
   //   enter: function (node, parent) {
   //     console.log("traverse-enter");
@@ -24,30 +24,58 @@ const execute = () => {
   // });
 
   const config = { ...utils.loadConfig(), ...overrides };
+  utils.init(config);
+
+  // Initial setup
   let targetFiles = config.files;
+  targetFiles = utils.keepFiles(targetFiles, "js");
 
   try {
-    // Initial setup
-    if(!config.skipSetup) {
-      utils.removeOutputFolder(config);
-      utils.createGitRepo(config);
-      targetFiles = utils.globAllFiles(config);
-    }
-    targetFiles = utils.keepFiles(targetFiles, "js");
-
     // Apply the rules to the files
-    let count = 1;
     const modified = {};
     targetFiles.forEach(filename => {
-      console.log(`Processing file ${count++} of ${targetFiles.length}`);
-      console.log(`filename = ${filename}`);
       const contents = fileio.readLines(filename);
       // modified[filename] = applyRulesToJS(contents, rules, filename);
 
+      const identifiers = [
+        "addClass",
+        "attr",
+        "byId",
+        "byClass",
+        "query",
+        "create",
+        "className",
+        "height", // ???
+        "innerHTML",
+        "className",
+        // "element["className"]     // You can do this as well
+        // "element[classNameFromVar]  // Or this
+        // "element.className.indexOf(someVar)
+        "style",
+        // "element.style.visibility = "visible" // For example
+        // "element.style.backgroundColor = "#F4FAB4" // For example
+        "getAttribute",
+        "setAttribute",
+        // "domConstruct.place(domConstruct.toDom(rawHtml), location)
+        "getElementsByTagName", // Might manipulate them later
+        // "document.body.style
+        "write",
+        "getElementById",
+        "createElement",
+        // var re = new RegExp('^sortTableBdy'); // Is this a class? Yes, but hard to programatically tell.
+        "getElementByClass",
+      ];
+
       // FIXME New AST stuff
-      let x1 = esprima.tokenize(contents.join("\n"));
-      x1 = x1.filter(item => item.type === "String");
-      console.log(`x1 = ${JSON.stringify(x1)}`);
+      let foundIdentifiers = esprima.tokenize(contents.join("\n"));
+      foundIdentifiers = foundIdentifiers.filter(item =>
+        item.type === "Identifier" &&
+        identifiers.indexOf(item.value) !== -1
+      );
+      if(foundIdentifiers.length > 0) {
+        console.info(`filename = ${filename}`);
+        console.info(`foundIdentifiers = ${JSON.stringify(foundIdentifiers)}`);
+      }
 
       // const x2 = esprima.parseScript(contents.join("\n"));
       // console.log(`x2 = ${JSON.stringify(x2)}`);
@@ -59,11 +87,11 @@ const execute = () => {
     // utils.writeFilesToDisk(config, ???);
 
   } catch (error) {
-    console.log(error);
+    console.error(error);
     process.exit(1);
   }
 
-  console.log("js-rules-tool finished");
+  console.info("js-rules-tool finished");
 }
 
 module.exports = { execute };
