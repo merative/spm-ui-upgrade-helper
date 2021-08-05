3ed50faee22a133080e4c8b50cc7d9c64d2f466a
Alan Bergin <alan.bergin@ie.ibm.com>
2021-08-05 16:13:09 +0100

    Trigger gatsby:deploy if changes made to packages/gatsby-docs folder

--- packages/gatsby-docs/gatsby-conditional-deploy.test.js
+++ packages/gatsby-docs/gatsby-conditional-deploy.test.js
@@ -26,6 +26,20 @@ test('it deploys if there are changes to the "docs" folder', () => {
   expect(expected.length).toEqual(dummyShellJs.execList.length);
 });
 
+test('it deploys if there are changes to the "packages/gatsby-docs" folder', () => {
+  const changes = [
+    "foo/foo.txt",
+    "packages/gatsby-docs/should-cause-deploy.txt",
+    "bar/bar.txt",
+  ];
+  const expected = [ "yarn gatsby:deploy" ];
+
+  gatsbyConditionalDeploy(dummyShellJs, changes);
+
+  expect(expected).toEqual(dummyShellJs.execList);
+  expect(expected.length).toEqual(dummyShellJs.execList.length);
+});
+
 test('it deploys if there are changes to nav-items.yaml', () => {
   const changes = [
     "foo/foo.txt",
@@ -40,7 +54,7 @@ test('it deploys if there are changes to nav-items.yaml', () => {
   expect(expected.length).toEqual(dummyShellJs.execList.length);
 });
 
-test('it does NOT deploy if there are no changes', () => {
+test('it does not deploy if there are no changes', () => {
   const changes = [
     "not-docs-folder/foo.txt",
   ]
