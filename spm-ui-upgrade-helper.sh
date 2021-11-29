93b56b2d621c61ea8c9566f3d2b66255e04c37cf
LUIZA-SALEK <luiza.salek@ibm.com>
2021-11-29 09:57:52 +0000

    chore(*): temporary changes

--- spm-ui-upgrade-helper.sh
+++ spm-ui-upgrade-helper.sh
@@ -46,8 +46,8 @@ docker-compose rm -f spm-ui-upgrade-helper
 echo Logging in to Docker Hub...
 docker login
 if [ "$?" != 0 ]; then echo "Error: Could not log in to Docker repo."; exit 1; fi
-docker pull ibmcom/spm-ui-upgrade-helper:$VERSION
-docker tag ibmcom/spm-ui-upgrade-helper:$VERSION  spm-ui-upgrade-helper:$VERSION 
+docker pull whgovspm/spm-ui-upgrade-helper:$VERSION
+docker tag whgovspm/spm-ui-upgrade-helper:$VERSION  spm-ui-upgrade-helper:$VERSION 
 docker-compose build 
 docker-compose run $DETACH_CMD -p 3000:3000 -p 4000:4000 \
     $UIUH_DEV_CMD \
