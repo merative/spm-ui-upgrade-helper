a6fef3b2686924244e9bee673c985274ae0c64eb
LUIZA-SALEK <luiza.salek@ibm.com>
2021-12-13 20:50:42 +0000

    chore(*): test3

--- spm-ui-upgrade-helper.sh
+++ spm-ui-upgrade-helper.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/sh
 
 ERROR=
 if [[ -z "$1" ]]; then
@@ -34,37 +34,23 @@ echo     VERSION = $VERSION
 echo     INPUT_FOLDER_CMD = $INPUT_FOLDER_CMD
 echo     OUTPUT_FOLDER_CMD = $OUTPUT_FOLDER_CMD
 echo     DETACH_CMD = $DETACH_CMD
-echo     TEST
+echo     
 
-
-docker stop spm-ui-upgrade-helper
-docker rm spm-ui-upgrade-helper
-docker stop spm-ui-upgrade-helper_beanparser
-docker rm spm-ui-upgrade-helper_beanparser
-docker stop spm-ui-upgrade-helper_nodefront
-docker rm spm-ui-upgrade-elper_nodefront
-docker image rm spm-ui-upgrade-helper_beanparser
-docker image rm spm-ui-upgrade-helper
-docker image rm spm-ui-upgrade-helper_nodefront
-docker image rm spm-ui-upgrade-helper_nodefron
-docker image rm whgovspm/spm-ui-upgrade-helper_beanparser
-docker image rm  whgovspm/spm-ui-upgrade-helper_nodefront
-docker image rm  whgovspm/spm-ui-upgrade-helper
+docker-compose rm -v -s -f
 
 echo Logging in to Docker Hub...
 docker login
 if [ "$?" != 0 ]; then echo "Error: Could not log in to Docker repo."; exit 1; fi
 docker pull whgovspm/spm-ui-upgrade-helper:$VERSION 
 docker tag whgovspm/spm-ui-upgrade-helper:$VERSION spm-ui-upgrade-helper
-echo done3
 docker pull whgovspm/spm-ui-upgrade-helper_nodefront:$VERSION
 docker tag whgovspm/spm-ui-upgrade-helper_nodefront:$VERSION spm-ui-upgrade-helper_nodefront
 docker pull whgovspm/spm-ui-upgrade-helper_beanparser:$VERSION
 docker tag whgovspm/spm-ui-upgrade-helper_beanparser:$VERSION spm-ui-upgrade-helper_beanparser
-docker image rm whgovspm/spm-ui-upgrade-helper_beanparser:$VERSION
-docker image rm  whgovspm/spm-ui-upgrade-helper_nodefront:$VERSION
-docker image rm  whgovspm/spm-ui-upgrade-helper:$VERSION
-docker-compose build 
+docker image rm -f whgovspm/spm-ui-upgrade-helper_beanparser:$VERSION
+docker image rm -f whgovspm/spm-ui-upgrade-helper_nodefront:$VERSION
+docker image rm -f whgovspm/spm-ui-upgrade-helper:$VERSION
+
 docker-compose run $DETACH_CMD -p 3000:3000 -p 4000:4000 \
     $UIUH_DEV_CMD \
     $INPUT_FOLDER_CMD \
@@ -72,3 +58,4 @@ docker-compose run $DETACH_CMD -p 3000:3000 -p 4000:4000 \
     --name spm-ui-upgrade-helper \
     spm-ui-upgrade-helper
 if [ "$?" != 0 ]; then echo "Error: Could not run $VERSION version."; exit 1; fi
+
